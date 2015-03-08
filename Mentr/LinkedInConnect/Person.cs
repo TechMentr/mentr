2371a678380ee45b9aede5fd0ab8b90a817be1b6
RituVyas <priyal.ritu@gmail.com>
2015-03-08 11:40:31 +0000

    Added logo and Person.cs

---
+++ Mentr/LinkedInConnect/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace LinkedInConnect
+{
+    [Serializable, XmlRoot("person")]
+    public class Person
+    {
+        [XmlElement("id")]
+        public string id { get; set; }
+
+        [XmlElement("first-name")]
+        public string FirstName { get; set; }
+
+        [XmlElement("last-name")]
+        public string LastName { get; set; }
+
+        [XmlElement("headline")]
+        public string headLine { get; set; }
+
+        [XmlElement("site-standard-profile-request")]
+        public string ProfileUrl { get; set; }
+
+        [XmlElement("picture-url")]
+        public string ImagerUrl { get; set; }
+    }
+}
